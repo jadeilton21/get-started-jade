aeaab2da8bdf6bca9525b900fec1dfcb82cd6508
jadeilton <jadeiltondm@gmail.com>
2025-03-23 15:12:45 -0300

    feat: create class PetMachine

--- src/PetMachine.java
+++ src/PetMachine.java
@@ -13,5 +13,42 @@ public class PetMachine {
     private Pet pet;
 
 
+    public void takeAShower(){
+        if (this.pet == null){
+            System.out.println("Coloque um pet");
+            return;
+        }
+        this.water -= 10;
+        this.shampoo -= 2;
+        pet.setClean(true);
+        System.out.println("O pet " + pet.getName() +" ESTÁ LIMPOOO");
+    }
+
+
+    public void addWater(){
+        if(water==30){
+
+            System.out.println("A capacidade de água da máquina está no máximo.");
+            return;
+        }
+        water+=2;
+    }
+    public void addS(){
+        if(water==10){
+
+            System.out.println("A capacidade de shampoo da máquina está no máximo.");
+            return;
+        }
+        shampoo+=2;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getShampoo() {
+        return shampoo;
+    }
+
 
 }
