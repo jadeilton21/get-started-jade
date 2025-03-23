347f7f08bc1bd9d0a537c2ee8f2da2568257fd0f
jadeilton <jadeiltondm@gmail.com>
2025-03-22 21:11:40 -0300

    feat: create class Pet

--- src/Pet.java
+++ src/Pet.java
@@ -6,6 +6,25 @@ public class Pet {
     private Boolean clean;
 
 
+    public Pet(String name, Boolean clean) {
+        this.name = name;
+        this.clean = clean;
+    }
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getClean() {
+        return clean;
+    }
+
+    public void setClean(Boolean clean) {
+        this.clean = clean;
+    }
 }
