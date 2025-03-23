36778d34b015a0a394f865566cff56ab0679158b
jadeilton <jadeiltondm@gmail.com>
2025-03-23 15:35:42 -0300

    feat

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
     public static void main(String[] args) {
+        var option = -1;
+        var scanner = new Scanner(System.in);
 
 
+        do{
+
+            System.out.println("------------- ESCOLHA UMA DAS OPÇÕES!!!!!");
+            System.out.println("1 - DAR BANHO NO PET");
+            System.out.println("2 - ABASTECER A MAQUINA COM AGUA");
+            System.out.println("3 - ABASTECER A MAQUINA COM SHAMPOO");
+            System.out.println("4 - VERIFICAR A AGUA DA MAQUINA");
+            System.out.println("5 - VERIFICAR O SHAMPOO DA MAQUINA");
+            System.out.println("6 - VERFICIAR SE TEM PET NO BANHO");
+            System.out.println("7 - COLOCAR PET NA MAQUINA");
+            System.out.println("8 - RETIRAR PET DA MAQUINA");
+            System.out.println("9 - LIMPAR A MAQUINA");
+            System.out.println("0 - SAIR");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 7 ->
+            }
+
+        }while (option != 0);
     }
+
 }
